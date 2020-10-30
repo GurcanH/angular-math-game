6039d7783e8a07349d9157ea7fae104e1cf50621
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-31 08:31:30 +1100

    258. Handling Correct Answers

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -28,7 +28,15 @@ export class EquationComponent implements OnInit {
 
   ngOnInit(): void {
     this.mathForm.statusChanges.subscribe((value) => {
-      console.log(value);
+      if (value === 'INVALID') {
+        return;
+      }
+
+      this.mathForm.setValue({
+        a: this.randomNumber(),
+        b: this.randomNumber(),
+        answer: '',
+      });
     });
   }
 
