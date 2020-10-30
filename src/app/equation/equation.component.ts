f2dda2b878a2b1c2e88df1c15c6991b62bf33320
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-31 07:59:47 +1100

    254. Adding Custom Validation

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
   styleUrls: ['./equation.component.css'],
 })
 export class EquationComponent implements OnInit {
-  mathForm = new FormGroup({
-    a: new FormControl(this.randomNumber()),
-    b: new FormControl(this.randomNumber()),
-    answer: new FormControl(''),
-  });
+  mathForm = new FormGroup(
+    {
+      a: new FormControl(this.randomNumber()),
+      b: new FormControl(this.randomNumber()),
+      answer: new FormControl(''),
+    },
+    [
+      (form: AbstractControl) => {
+        const { a, b, answer } = form.value;
+        if (a + b === parseInt(answer)) {
+          return null;
+        }
+        return { addition: true };
+      },
+    ]
+  );
   constructor() {}
 
   get a() {
